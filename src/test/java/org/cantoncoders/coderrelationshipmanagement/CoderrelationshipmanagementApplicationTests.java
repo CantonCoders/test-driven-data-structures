2e1d67d79775033962dbd350da7edcfc8ff36128
Joel <joel.t.norman@gmail.com>
2022-04-25 10:49:44 -0400

    assertj added

--- src/test/java/org/cantoncoders/coderrelationshipmanagement/CoderrelationshipmanagementApplicationTests.java
+++ src/test/java/org/cantoncoders/coderrelationshipmanagement/CoderrelationshipmanagementApplicationTests.java
@@ -3,11 +3,14 @@ package org.cantoncoders.coderrelationshipmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class CoderrelationshipmanagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(false).isFalse();
 	}
 
 }
