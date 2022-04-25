adfef053d96ee6d012806377978d9f610a212039
Joel <joel.t.norman@gmail.com>
2022-04-25 10:42:18 -0400

    Spring Boot Initializer

---
+++ src/main/java/org/cantoncoders/coderrelationshipmanagement/CoderrelationshipmanagementApplication.java
@@ -0,0 +1,13 @@
+package org.cantoncoders.coderrelationshipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderrelationshipmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderrelationshipmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cantoncoders/coderrelationshipmanagement/CoderrelationshipmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cantoncoders.coderrelationshipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderrelationshipmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
